3d338549841776683ecb5397a076bf7ad6d8ea6f
David Regan <dregan1@comcast.net>
2017-11-08 09:04:59 -0600

    journal table made saving devo

--- app/factories/homeFactory.js
+++ app/factories/homeFactory.js
@@ -3,42 +3,40 @@ app.factory('homeFactory', function ($http, $q) {
   const baseURL = 'http://localhost:1147/api/v1/';
   return {
 
-   getBooks: function() {
+    getBooks: function () {
       return $http({
         method: 'GET',
         url: baseURL + 'book',
-      
+
       });
     },
 
-    getTexts: function(version, book) {
+    getTexts: function (version, book) {
       return $http({
         method: 'GET',
-        url: baseURL + 'text?version='+ version +'&book='+book,
+        url: baseURL + 'text?version=' + version + '&book=' + book,
       });
     },
 
-getChapters: function(book) {
-  console.log("bookfromgetchap", book);
-  return $http({
-    method: 'GET',
-    url: baseURL + 'chapter/'+ book,
-    
-
- });
-  
-  },
+    getChapters: function (book) {
+      console.log("bookfromgetchap", book);
+      return $http({
+        method: 'GET',
+        url: baseURL + 'chapter/' + book,
 
 
-getVersions: function() {
-  return $http({
-    method: 'GET',
-    url: baseURL + 'version',
+      });
 
- });
-  
+    },
+
+
+    getVersions: function () {
+      return $http({
+        method: 'GET',
+        url: baseURL + 'version',
+
+      });
+
+    }
   }
-}
 });
-
-
